160d3b1ff7526916b6d222a94c29d6228190ba93
kavindu <kavindupath@gmail.com>
2020-06-11 00:11:30 +0530

    initalize redux app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,18 @@
+export default function(){
+
+    return(
+        [
+            {title: "Harry Potter"},
+
+            {title: "Lord of the Rings"},
+
+            {title: "Game of thrones"},
+
+            {title: "Sherlock"},
+
+            {title: "Prison break"},
+
+        ]
+
+    )
+}
